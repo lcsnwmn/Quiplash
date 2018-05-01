bacef854b1c63417f1510b9049fba8d8089662c3
BrandonHansen <bhansen4@nd.edu>
2018-05-01 13:03:16 -0400

    added main page functionality

--- consoleClient/scripts/main.js
+++ consoleClient/scripts/main.js
@@ -3,20 +3,21 @@
 var states = ["lobby", "prompt", "answers", "tally"];
 var currentState = states[0];
 
-var server_url = "http://student04.cse.nd.edu:9898";
+var server_url = "http://student01.cse.nd.edu:9898";
 var max_players = 4;
 
 function testFunction() {
 	var xhr = new XMLHttpRequest();
 	xhr.onreadystatechange = function() {
 		if (xhr.status == 200) {
-			document.getElementById("test").innerHTML = xhr.responseText;
+			var jsonObj = JSON.parse(xhr.responseText);
+			document.getElementById("test").innerHTML = jsonObj['result'];
 		}
 		else{
 			document.getElementById("test").innerHTML = xhr.status;
 		}
 	}
-	xhr.open("GET",  server_url+"/players/", true);
+	xhr.open("GET",  server_url+"/gamestate", true);
 	xhr.send();
 }
 
@@ -26,24 +27,49 @@ function getPlayerName() {
 
 	document.getElementById("test").innerHTML = "Worked";
 	var xhr = new XMLHttpRequest();
-    	xhr.open("GET",  server_url+"/players/", true);
 	xhr.onload = function() {
-		if (xhr.status == 500) {
-			document.getElementById("test").innerHTML = "Failed";
-		}
-		else {
-			player_stats = JSON.parse(xhr.responseText);
-		}
 		if (xhr.readyState == 4 && xhr.status == 200) {
-			document.getElementById("title").innerHTML =
-			xhr.responseText;
+			var jsonObj = JSON.parse(xhr.responseText);
+			document.getElementById("test").innerHTML = jsonObj['result'];
+			for (var index = 1; index <= 4; index++) {
+				var pnum = index.toString();
+				document.getElementById("p"+pnum).innerHTML = jsonObj['result'];
+				console.log(jsonObj["p"+pnum]);
+			}
+		}
+		else{
+			document.getElementById("test").innerHTML = xhr.status;
 		}
 		
 	}
+	xhr.open("GET",  server_url+"/players", true);
+	return xhr.send();
 }
 
 function checkStateChange(){
 
+
+	var xhr = new XMLHttpRequest();
+	xhr.onreadystatechange = function() {
+		if (xhr.status == 200) {
+			var jsonObj = JSON.parse(xhr.responseText);
+			document.getElementById("test").innerHTML = jsonObj['result'];
+			document.getElementById(state).className = "inactive";
+			document.getElementById(newState).className = "active";
+		}
+		else{
+			document.getElementById("test").innerHTML = xhr.status;
+		}
+	}
+	var players = getPlayerName();
+	console.log(players);
+	for (var index = 1; index >= 4; index++) {
+		var pnum = index.toString();
+		document.getElementById("p"+pnum).innerHTML = jsonObj['result'];
+	}
+	/*
+	xhr.open("GET",  server_url+"/gamestate", true);
+	xhr.send();
 	var statesLength = states.length;
 	for (var index = statesLength-1; index >= 0; index--) {
 		state = states[index];
@@ -56,7 +82,7 @@ function checkStateChange(){
 			break;
 		}
 	}
-/*	var xhr = new XMLHttpRequest();
+	var xhr = new XMLHttpRequest();
 	xhr.onreadystatechange = function() {
 
 		
@@ -67,7 +93,7 @@ function checkStateChange(){
 }
 
 
-var intervalID = setInterval(checkStateChange, 3000);
+//var intervalID = setInterval(checkStateChange, 3000);
 
 
 
