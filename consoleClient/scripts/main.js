5c4a5ec54db2a7da1602f0a92c633eefa9d986a0
Matthew Fabian <mfabian1@nd.edu>
2018-05-08 10:01:35 -0400

    console lobby code done

--- consoleClient/scripts/main.js
+++ consoleClient/scripts/main.js
@@ -1,8 +1,9 @@
 
 
-var states = ["lobby", "prompt", "answers", "tally"];
-var currentState = "lobby";
+var states = ["lobby", "prompts", "answers", "tally"];
+var currentState = "none";
 var timer_count = 0;
+
 
 var round = 1;
 
@@ -11,38 +12,39 @@ var max_players = 4;
 var server_url = "http://student01.cse.nd.edu:9898";
 
 
-var intervalID = setInterval(checkStateChange, 1000);
-
+var intervalID = setInterval(getState, 1000);
 
 //GET GAME STATE FROM SERVER AND CHANGE MAIN PAGE TO STATE
-function checkStateChange(){
+function getState(){
 	
 	var xhr = new XMLHttpRequest();
 	xhr.onreadystatechange = function() {
-		if (xhr.status == 200) {
+		if ((xhr.status == 200)&&(xhr.responseText != null)) {
+			//console.log(xhr.responseText)
 			
 			//GET GAMESTATE
-			var gamestate = 'prompt';//JSON.parse(xhr.responseText)['message']['gamestate'];
+			var gamestate = JSON.parse(xhr.responseText)["result"]
 			//CHECK IF STATE CHANGED
 			if (currentState != gamestate) {
-				
-				//IF STATE CHANGED ASSIGN NEW STATE
-				//RESET TIMER AND SWITCH ACTIVE OF STATES TO MAKE APPEAR AND DISAPEAR
-				timer_count = 0;
-				document.getElementById(currentState).className = "inactive";
-				currentState = gamestate;
-				document.getElementById(currentState).className = "active";
+				if (gamestate != "error"){
+					//IF STATE CHANGED MAKE VISIBLE
+					if (currentState != "none"){
+						document.getElementById(currentState).className = "inactive";
+					}
+					document.getElementById(gamestate).className = "active";
+					currentState = gamestate;
+				}
 			}
 			else {
 				//IF STATE NOT CHANGED DO ACTIVITY OF STATE
 				stateFunctions();
 			}
 		}
-		else{
-			console.log("fail1");
+		else if (xhr.status != 0){
+			console.log("Gamestate Error:" + xhr.status);
 		}
 	}
-	xhr.open("GET",  server_url+"/gamestate", true);
+	xhr.open("GET",  server_url+"/gamestate", false);
 	xhr.send();
 }
 
@@ -52,14 +54,16 @@ function stateFunctions() {
 	if (currentState == "lobby") {
 		lobbyWait();
 	}
-	else if (currentState == "prompt") {
-		getPrompt();
+	else if (currentState == "prompts") {
+		//getPrompt();
 	}
 	else if (currentState == "answers") {
-		getAnswers();
+		//getAnswers();
 	}
-	else {
-		tallyScore();
+	else if (currentState == "tally"){
+		//tallyScore();
+	}else{
+		//console.log("Error with current state: current state is " + currentState)
 	}
 }
 
@@ -68,17 +72,47 @@ function getPlayers() {
 	var xhr = new XMLHttpRequest();
 	xhr.onload = function() {
 		if (xhr.status == 200) {
-			
+			//console.log(xhr.responseText)
 			//LOOP THROUGH PLAYER INDEXES AND GET PLAYER NAME AND SCORE
-			var players = JSON.parse(xhr.responseText)['message'];
-			for (var index = 1; index <= max_players; index++) {
-				var pnum = index.toString();
-				document.getElementById("p"+pnum).innerHTML = players[pnum]["name"];
-				document.getElementById("s"+pnum).innerHTML = players[pnum]["score"];
+			var players = JSON.parse(xhr.responseText);
+			console.log(players)
+			if (players["result"] == "success"){
+				// Add players to table
+				for (var index = 0; index < max_players; index++) {
+					var pnum = index.toString();
+					if (players[pnum] != undefined){
+						
+						console.log(pnum + " " + players[pnum])
+
+						var $table=document.getElementById('player_table');
+						if ($table.rows.length < index + 2){
+							var row=$table.insertRow(-1);
+							var cell1=row.insertCell(0);
+							var cell2=row.insertCell(1);
+							var cell3=row.insertCell(2);
+							cell1.innerHTML=players[pnum]
+							cell2.innerHTML=0
+							if (pnum == "0"){
+								cell3.innerHTML = '<img id="avatar" src="images/cube_boy_large.gif" alt="Cube Boy">'
+							}else if (pnum == "1"){
+								cell3.innerHTML = '<img id="avatar" src="images/sphere_kid_large.gif" alt="Sphere Kid">'
+							}else if (pnum == "2"){
+								cell3.innerHTML = '<img id="avatar" src="images/green_thing_large.gif" alt="Green Thing">'
+							}else if (pnum == "3"){
+								cell3.innerHTML = '<img id="avatar" src="images/the_cone_large.gif" alt="The Cone">'
+							}
+						}
+					}
+				}
+
+				// Check if game is full
+				if ($table.rows.length > max_players){
+					change_state("prompts")
+				}
 			}
 		}
 		else{
-			console.log("fail2");
+			console.log("Player Error:" + xhr.status);
 		}
 		
 	}
@@ -89,15 +123,34 @@ function getPlayers() {
 
 //WIATING WIDGET (NOT IMPORTANT)
 function lobbyWait() {
-		var currentText = "Waiting for Players.";
+		var currentText = "Waiting for Players...";
+		/*
 		for (var increment = 0; increment < (timer_count%max_players); increment++) {
 			currentText = currentText + '.';
 		}
 		document.getElementById("lobby_timer_text").innerHTML = currentText;
-		timer_count = timer_count + 1;
+		timer_count = timer_count + 1; 
+		*/
 		getPlayers();
 }
 
+// Change the gamestate
+function change_state(state){
+	var xhr = new XMLHttpRequest();
+	xhr.onload = function () {
+		if (xhr.status == "200") {
+			console.log("State Changed to " + state);
+		} else {
+			console.error("State Change Error (" + state + "): " + xhr.status);
+		}
+	}
+	xhr.open("PUT",  server_url+"/gamestate", true);
+	var request = '"' + state + '"'
+	//console.log(request)
+	xhr.send(request);
+}
+
+/*
 //RETRIEVE QUESTION PROMPT FROM SERVER
 function getPrompt() {
 	
@@ -217,3 +270,4 @@ function tallyScore() {
 	getPlayers();
 }
 
+*/
